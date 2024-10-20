a08068c8ec703e8d0b361d5703f15fe04f8309ae
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2024-10-21 00:25:50 +0900

    project: Add Script/install_took.mk to build console application

--- Script/install_tool.mk
+++ Script/install_tool.mk
@@ -6,6 +6,9 @@ PRODUCT_DIR	= ../Product
 
 all: unittest
 
+clean:
+	(cd $(PRODUCT_DIR) && rm -rf $(BUNDLE_PATH))
+	
 unittest: install_bundle
 	xcodebuild install \
  	  -scheme UnitTest \
@@ -20,7 +23,6 @@ unittest: install_bundle
 	  ONLY_ACTIVE_ARCH=NO
 
 install_bundle: dummy
-	(cd $(PRODUCT_DIR) && rm -rf $(BUNDLE_PATH))
 	xcodebuild install \
 	  -scheme Bundle \
 	  -project $(PROJECT_NAME).xcodeproj \
@@ -35,63 +37,3 @@ install_bundle: dummy
 
 dummy:
 
-
-# BIN_PATH	= binary/bin
-# 
-# all: dummy
-# 	echo "usage: make install_bundle or make install_tools"
-# 
-# install_jsh_bundle: dummy
-# 	xcodebuild install \
-# 	  -scheme jsh_bundle \
-# 	  -project $(PROJECT_NAME).xcodeproj \
-# 	  -destination="macOSX" \
-# 	  -configuration Release \
-# 	  -sdk macosx \
-# 	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-# 	  INSTALL_PATH=$(BUNDLE_PATH) \
-# 	  SKIP_INSTALL=NO \
-# 	  DSTROOT=$(PRODUCT_DIR) \
-# 	  ONLY_ACTIVE_ARCH=NO
-# 
-# install_edecl: dummy
-# 	xcodebuild install \
-# 	  -scheme edecl \
-# 	  -project $(PROJECT_NAME).xcodeproj \
-# 	  -destination="macOSX" \
-# 	  -configuration Release \
-# 	  -sdk macosx \
-# 	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-# 	  INSTALL_PATH=$(BIN_PATH) \
-# 	  SKIP_INSTALL=NO \
-# 	  DSTROOT=$(PRODUCT_DIR) \
-# 	  ONLY_ACTIVE_ARCH=NO
-# 
-# install_jsrun: dummy
-# 	xcodebuild install \
-# 	  -scheme jsrun \
-# 	  -project $(PROJECT_NAME).xcodeproj \
-# 	  -destination="macOSX" \
-# 	  -configuration Release \
-# 	  -sdk macosx \
-# 	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-# 	  INSTALL_PATH=$(BIN_PATH) \
-# 	  SKIP_INSTALL=NO \
-# 	  DSTROOT=$(PRODUCT_DIR) \
-# 	  ONLY_ACTIVE_ARCH=NO
-# 
-# install_jsh: dummy
-# 	xcodebuild install \
-# 	  -scheme jsh \
-# 	  -project $(PROJECT_NAME).xcodeproj \
-# 	  -destination="macOSX" \
-# 	  -configuration Release \
-# 	  -sdk macosx \
-# 	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-# 	  INSTALL_PATH=$(BIN_PATH) \
-# 	  SKIP_INSTALL=NO \
-# 	  DSTROOT=$(PRODUCT_DIR) \
-# 	  ONLY_ACTIVE_ARCH=NO
-# 
-# dummy:
-# 
